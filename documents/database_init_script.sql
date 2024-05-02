abb22b7404e90a40c8bb23f5a2226fc8b2b42522
Antonino Ciancimino <antonino.ciancimino@mail.polimi.it>
2024-05-02 14:29:48 +0200

    Update database creation script, each entity now has an integer primary key

--- documents/database_init_script.sql
+++ documents/database_init_script.sql
@@ -18,53 +18,56 @@ CREATE TABLE person (
 );
 
 CREATE TABLE image (
+	id INTEGER NOT NULL,
 	file_path VARCHAR(255) NOT NULL,
 	title VARCHAR(30) NOT NULL,
     upload_date DATE NOT NULL,
     uploader_id INTEGER NOT NULL,
     
+    UNIQUE (file_path),
     FOREIGN KEY (uploader_id) REFERENCES person(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE,
-	PRIMARY KEY (file_path)
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE album (
+	id INTEGER AUTO_INCREMENT NOT NULL,
 	title VARCHAR(30) NOT NULL,
     creation_date DATE NOT NULL,
     creator_id INTEGER NOT NULL,
     
-    foreign KEY (creator_id) REFERENCES person(id)
+   	CONSTRAINT album_unique UNIQUE (id, creator_id),
+    FOREIGN KEY (creator_id) REFERENCES person(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE,
-    PRIMARY KEY (title, creator_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE image_album (
-	image_path VARCHAR(255),
-    album_title VARCHAR(30),
-    album_creator_id INTEGER NOT NULL,
+	image_id INTEGER,
+    album_id INTEGER,
     order_position INTEGER,
     
-    FOREIGN KEY (image_path) REFERENCES image(file_path)
+    FOREIGN KEY (image_id) REFERENCES image(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
-    FOREIGN KEY (album_title, album_creator_id) REFERENCES album(title, creator_id)
+    FOREIGN KEY (album_id) REFERENCES album(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
-    PRIMARY KEY (image_path, album_title, album_creator_id)
+    PRIMARY KEY (image_id, album_id)
 );
 
 CREATE TABLE text_comment (
 	id INTEGER AUTO_INCREMENT NOT NULL,
     content TEXT NOT NULL,
-    image_path VARCHAR(255) NOT NULL,
+    image_id INTEGER NOT NULL,
     author_id INTEGER,
     
-    foreign KEY (image_path) REFERENCES image(file_path)
+    FOREIGN KEY (image_id) REFERENCES image(id)
 		ON UPDATE CASCADE
         ON DELETE CASCADE,
-    foreign KEY (author_id) REFERENCES person(id)
+    FOREIGN KEY (author_id) REFERENCES person(id)
 		ON UPDATE CASCADE
         ON DELETE SET NULL,
 	PRIMARY KEY (id)
