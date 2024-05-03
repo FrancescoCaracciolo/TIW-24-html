4ed34d4986e6fa3e99ac61b8311f50f764d6c591
Antonino Ciancimino <antonino.ciancimino@mail.polimi.it>
2024-05-03 15:38:36 +0200

    Update LogoutServlet

--- src/main/java/it/polimi/tiw/servlets/LogoutServlet.java
+++ src/main/java/it/polimi/tiw/servlets/LogoutServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @WebServlet("/logout")
 public class LogoutServlet extends DataServlet {
@@ -17,14 +16,11 @@ public class LogoutServlet extends DataServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		session.setAttribute("Id", null);
+		request.getSession().invalidate();
 		response.sendRedirect("signin");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		session.setAttribute("Id", null);
-		response.sendRedirect("signin");
+		doGet(request, response);
 	}
 }
